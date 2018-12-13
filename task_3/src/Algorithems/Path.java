cd7d8a96557660678620410937ba06cd476505a2
shiloygilor <shilo.y.gilor@gmail.com>
2018-12-13 08:58:21 +0200

    path

--- task_3/src/Algorithems/Path.java
+++ task_3/src/Algorithems/Path.java
@@ -14,6 +14,9 @@ public class Path {
 	Packman my_packman;
 	double time = 0.0;
 	ArrayList<Point3D> locations = new ArrayList<Point3D>();
+	MyCoords cord = new MyCoords();
+	Algorithems algo = new Algorithems();
+
 	public Path(Packman my_packman) {
 		super();
 		this.my_packman = my_packman;
@@ -32,7 +35,6 @@ public class Path {
 	}
 	public double get_total_distance()
 	{
-		MyCoords mycoords = new MyCoords();
 		double total_distance = 0.0;
 		Iterator<Point3D> iter = locations.iterator();
 		Point3D current_location = iter.next();
@@ -40,7 +42,7 @@ public class Path {
 		{
 			//Point3D temp_location = algorithems.edge_until_eat(current_location, iter.next(), my_packman.getRange());
 			Point3D temp_location = iter.next();
-			total_distance += mycoords.distance3d(current_location,temp_location);
+			total_distance += cord.distance3d(current_location,temp_location);
 			current_location = temp_location;
 		}
 		return total_distance;
@@ -58,6 +60,39 @@ public class Path {
 		return temp_path;
 		
 	}
+	public double get_time_between_2_points(Point3D start , Point3D end)
+	{
+		return cord.distance3d(start, end)/my_packman.getSpeed();
+	}
+	public Point3D get_location_by_time(Double time)
+	{
+		double time_left = time;
+		double temp_time;
+		Point3D temp_location = locations.get(0);
+		for (int i =0 ; i<locations.size()-1;i++)
+		{
+			temp_time = get_time_between_2_points(locations.get(i) ,locations.get(i+1));
+			if (temp_time<time_left)
+			{
+				time_left = time_left - temp_time;
+			}
+			else
+			{
+				Point3D meters_start = algo.convert_gps_to_meters(locations.get(i));
+				Point3D meters_end = algo.convert_gps_to_meters(locations.get(i+1));
+				Point3D vect = new Point3D(meters_end.x() - meters_start.x() , meters_end.y() - meters_start.y() , meters_end.z() - meters_start.z());
+				double totalD =  Math.sqrt(vect.x()*vect.x() + vect.y()*vect.y() + vect.z()*vect.z());
+				double t = time_left/temp_time;
+				Point3D newvec = new Point3D(vect.x()*t , vect.y()*t ,vect.z()*t);
+				Point3D final_point_meters = new Point3D(meters_start.x()+newvec.x() ,meters_start.y()+newvec.y() , meters_start.z()+newvec.z());
+				return algo.convert_meters_to_gps(final_point_meters);	
+			}
+		}
+		return locations.get(locations.size()-1);
+		
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Path [algorithems=" + algorithems + ", my_packman=" + my_packman + ", time=" + time + ", locations="
